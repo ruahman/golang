6911c158a54d267ef5816e9374cbf34c221a3f2f
diego vila <ruahman@gmail.com>
2022-05-21 23:41:58 -0400

    change it

---
+++ func/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// call this at the end
+	defer LastHi()
+
+	// push on defer stack
+	defer func() {
+		fmt.Println("Almost last??")
+	}()
+
+	a := HelloStr
+
+	fmt.Println(a())
+
+	b := TwoValues
+
+	x, y := b()
+
+	fmt.Println(x, y)
+}
+
+func HelloStr() string {
+	return "Hello String"
+}
+
+func TwoValues() (string, string) {
+	return "string2", "string2"
+}
+
+func LastHi() {
+	fmt.Println("Last Hi")
+}
