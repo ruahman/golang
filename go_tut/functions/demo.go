ed6e732367a536f89e786280310f4e708252c944
diego vila <ruahman@gmail.com>
2022-07-11 19:57:08 -0400

    add map

--- go_tut/functions/demo.go
+++ go_tut/functions/demo.go
@@ -10,10 +10,23 @@ func multiple_return() (int, bool) {
 	return 123, false
 }
 
+func lastHi() {
+	fmt.Println("lastHi")
+}
+
 func Demo() {
 	card := newCard()
 	i, b := multiple_return()
-  fmt.Println("multiple retrun: ", i, b)
+
+	// call this at the end
+	defer lastHi()
+
+	// push on defer stack
+	defer func() {
+		fmt.Println("Almost last??")
+	}()
+
+	fmt.Println("multiple retrun: ", i, b)
 	fmt.Println(card)
 	fmt.Println("demo from functions")
 }
