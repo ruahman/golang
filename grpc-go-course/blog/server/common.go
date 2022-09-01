f13d1a5da5f82b4894df7d00714790ab21177aac
diego vila <ruahman@gmail.com>
2022-09-01 13:15:15 -0400

    got ride of grpc

--- grpc-go-course/blog/server/common.go
+++
@@ -1,5 +0,0 @@
-package main
-
-import "go.mongodb.org/mongo-driver/mongo"
-
-var collection *mongo.Collection
--- grpc-go-course/blog/server/server.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import pb "github.com/Clement-Jean/grpc-go-course/blog/proto"
-
-type Server struct {
-	pb.BlogServiceServer
-}
