56e5be0f2200c23f18283cd22f9d9e44cebfc2d1
Diego Vila <diego@velascommerce.com>
2022-11-13 23:14:58 -0400

    got ride of the go docker stuff

--- go_docker/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var a = "test"
-	fmt.Println(a)
-	fmt.Println("hello world")
-}
--- go_tut/main.go
+++ go_tut/main.go
@@ -1,43 +1,49 @@
 package main
 
 import (
-	"go_tut/arrays"
-	"go_tut/channels"
-	"go_tut/conditions"
-	"go_tut/custom_type"
-	"go_tut/err"
-	"go_tut/functions"
-	"go_tut/generics"
-	"go_tut/goroutines"
-	"go_tut/interfaces"
-	"go_tut/loops"
-	"go_tut/maps"
-	"go_tut/pointers"
-	"go_tut/ranges"
-	"go_tut/slices"
-	"go_tut/strings"
-	"go_tut/structs"
-	"go_tut/switches"
+	// "go_tut/arrays"
+	// "go_tut/channels"
+	// "go_tut/conditions"
+	// "go_tut/custom_type"
+	// "go_tut/err"
+	// "go_tut/functions"
+	// "go_tut/generics"
+	// "go_tut/goroutines"
+	// "go_tut/interfaces"
+	// "go_tut/loops"
+	// "go_tut/maps"
+	// "go_tut/pointers"
+	// "go_tut/ranges"
+	// "go_tut/slices"
+	// "go_tut/strings"
+	// "go_tut/structs"
+	// "go_tut/switches"
+	// "go_tut/channels"
+	// "go_tut/err"
+	// "go_tut/goroutines"
+	// "go_tut/channels"
+	// "go_tut/err"
+	// "go_tut/goroutines"
 	"go_tut/variables"
 )
 
 func main() {
 	variables.Demo()
-	conditions.Demo()
-	arrays.Demo()
-	slices.Demo()
-	loops.Demo()
-	custom_type.Demo()
-	switches.Demo()
-	strings.Demo()
-	ranges.Demo()
-	functions.Demo()
-	structs.Demo()
-	interfaces.Demo()
-	maps.Demo()
-	pointers.Demo()
-	generics.Demo()
-	err.Demo()
-	goroutines.Demo()
-	channels.Demo()
+	// conditions.Demo()
+	// arrays.Demo()
+	// slices.Demo()
+	// loops.Demo()
+	// custom_type.Demo()
+	// switches.Demo()
+	// strings.Demo()
+	// ranges.Demo()
+	// functions.Demo()
+	// structs.Demo()
+	// interfaces.Demo()
+	// maps.Demo()
+	// pointers.Demo()
+	// generics.Demo()
+	// err.Demo()
+	// goroutines.Demo()
+	// channels.Demo()
 }
