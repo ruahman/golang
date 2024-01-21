1927c255faf2ad0e15c9ed61900d07b87e0a91c8
ruahman <dego_vila@yahoo.com>
2024-01-20 21:36:26 -0400

    go main

---
+++ go_tut/README.md
@@ -0,0 +1,14 @@
+
+# go run 
+`go run ./main.go`
+
+# go test 
+`go test -v <package-name>`
+- v: is for verbose
+
+# package 
+a package is like a __project__, or __workspace__.
+
+`package main` is making an executable package.
+
+`package <name>` is making a reusable package that can be imported an used as a dependenciy.
--- go_tut/main.go
+++ go_tut/main.go
@@ -1,44 +1,7 @@
 package main
 
-import (
-	"go_tut/cli_args"
-	"go_tut/hello_world"
-	"go_tut/variables"
-	"log"
-	"os"
-
-	"github.com/urfave/cli/v2"
-)
+import "fmt"
 
 func main() {
-	app := &cli.App{
-		Name:  "My golang Tutorial",
-		Usage: "This is a tutorial for golang",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{
-				Name:    "variables",
-				Aliases: []string{"v"},
-			},
-			&cli.BoolFlag{
-				Name:    "cli",
-				Aliases: []string{"c"},
-			},
-		},
-		Action: func(ctx *cli.Context) error {
-			if ctx.Bool("variables") {
-				variables.Run()
-			} else if ctx.Bool("help") {
-				cli.ShowAppHelp(ctx)
-			} else if ctx.Bool("cli") {
-				cli_args.Exec()
-			} else {
-				hello_world.Run()
-			}
-			return nil
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Hello, World!")
 }
