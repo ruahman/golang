ade6526c5a2346250b7d4ac67687ea471d0768d8
Diego Vila <diego@velascommerce.com>
2024-03-06 08:15:44 -0400

    go templates

--- go_tut/templates/templates.go
+++ go_tut/templates/templates.go
@@ -3,25 +3,38 @@ package templates
 import (
 	"log"
 	"os"
+	"strings"
 	"text/template"
 )
 
 func Run() {
-	// t, err := template.New("test").Parse("Hello, {{.Name}}!")
-	t, err := template.ParseFiles("template.gohtml")
+	funcMap := template.FuncMap{
+		"lower": strings.ToLower,
+		"upper": strings.ToUpper,
+		"cus": func(s string) string {
+			return "cus: " + s
+		},
+	}
+	t, err := template.New("template.gohtml").Funcs(funcMap).ParseFiles("template.gohtml")
 	if err != nil {
 		log.Fatal("Parse: ", err)
 	}
-	// data := struct {
-	// 	Name string
-	// }{
-	// 	Name: "World",
-	// }
-	// if err := t.Execute(os.Stdout, nil); err != nil {
-	// 	log.Fatal("Execute: ", err)
-	// }
+
 	slices := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
-	if err := t.ExecuteTemplate(os.Stdout, "template.gohtml", slices); err != nil {
+	maps := map[string]string{
+		"Japan":  "Tokyo",
+		"China":  "Beijing",
+		"Canada": "Ottawa",
+		"USA":    "Washington D.C.",
+	}
+
+	type Data struct {
+		Slices []string
+		Maps   map[string]string
+	}
+	var data Data = Data{slices, maps}
+
+	if err := t.ExecuteTemplate(os.Stdout, "template.gohtml", data); err != nil {
 		log.Fatal("Execute: ", err)
 	}
 }
