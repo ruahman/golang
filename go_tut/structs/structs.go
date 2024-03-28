5a583ef1362613c4371af0966fc9e686fd4a2c0f
Diego Vila <diego@velascommerce.com>
2024-03-28 19:29:46 -0400

    slices

--- go_tut/structs/structs.go
+++ go_tut/structs/structs.go
@@ -1,6 +1,9 @@
 package structs
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // struct is a collection of fields
 
@@ -146,4 +149,43 @@ func Demo() {
 	// can also calify for interfaces ment for the embedded struct
 	var d describer = co
 	d.describe()
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// interface resolution is implicit
+type Shape interface {
+	Area() float64
+}
+
+// every method has a receiver
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
