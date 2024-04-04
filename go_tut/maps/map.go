cd3cfab020f88caa0e1433d8292f5c5dae155927
Diego Vila <diego@velascommerce.com>
2024-04-04 08:09:57 -0400

    maps

--- go_tut/maps/map.go
+++ go_tut/maps/map.go
@@ -2,7 +2,10 @@ package maps
 
 // maps are just key maps of values
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func printMap(c map[string]string) {
 	// this is how you iterate over a map
@@ -10,6 +13,37 @@ func printMap(c map[string]string) {
 	for key, val := range c {
 		fmt.Println(key, val)
 	}
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
 
 func Run() {
