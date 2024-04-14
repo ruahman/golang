1d1abd04b05f8ace1055c9843fd74334b6b0714a
Diego Vila <diego@velascommerce.com>
2024-04-14 08:06:37 -0400

    mocking and concurrency

---
+++ go_tut/mocking/moocking.go
@@ -0,0 +1,30 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
